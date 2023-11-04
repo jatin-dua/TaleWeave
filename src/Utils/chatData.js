b4782d734594b5ce2af481e1db565776d802ed86
Swetanshu <swetanshusingh17@gmail.com>
2023-11-04 23:58:02 +0530

    Add Chat Retrival Functionality to GameScreen

---
+++ src/Utils/chatData.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const chatData = [
+    {   id : uuidv4(),
+        textData : "The year is 2043. Dr. Ava Miller, a brilliant scientist at MIT, has just completed her life's work: a working time machine.Ava is excited and nervous at the same time. She knows that her invention could change the world, but she's also worried about the potential consequences. What if she falls into the wrong hands? What if she accidentally alters the past and creates a new timeline?Despite the risks, Ava decides to test her time machine. She sets the coordinates for November 4, 2023, and steps inside. The machine whirs and hums, and Ava feels herself being pulled through time and space.When she opens her eyes, she's standing in her own living room, but something is different. The furniture is old and dusty, and there's a thick layer of dust on the floor. Ava realizes that she's traveled back in time to the year 2023.Ava is excited to explore the past, but she knows that she has to be careful. She can't change anything that hasn't already happened, or she could create a paradox. But she can learn from the past and use that knowledge to make the future a better place.",
+        img : false
+    },
+    {
+        id : uuidv4(),
+        textData : "In the quiet town of Willowbrook, nestled amidst rolling hills and meandering streams, a peculiar occurrence was about to unfold. A thick veil of mist enveloped the cobblestone streets, casting an eerie ambiance over the usually serene neighborhood. The townspeople, typically engrossed in their daily routines, now found themselves drawn to the mysterious phenomenon. Whispers traveled through the air, carrying tales of enchantment and unseen forces at play. As the mist swirled and danced, a sense of anticipation mingled with trepidation, weaving its way into the hearts of the townsfolk. Curiosity took hold, and one by one, they emerged from their cozy homes, their eyes wide with anticipation. Among them was young Amelia, a spirited adventurer with an insatiable thirst for the unknown. With her trusty journal in hand and a glimmer of excitement in her eyes, she ventured into the mist, her heart pounding with a mix of curiosity and a hint of fear. Little did she know that this peculiar event would mark the beginning of an extraordinary journey that would forever change her life and the destiny of Willowbrook.",
+        img : false
+    },
+    {
+        id : uuidv4(),
+        textData : "Once upon a time, there was a young girl named Alice who loved to explore the old manor house where she lived. One day, while she was wandering through the attic, she stumbled upon a hidden room. Inside the room was a large mirror, unlike any Alice had ever seen before.As Alice stepped closer to the mirror, she saw her own reflection, but something was different. Her reflection was older and wiser, with a twinkle in her eye. Alice reached out to touch the mirror, but as soon as her fingers made contact, she felt a strange tingling sensation.Suddenly, Alice found herself transported to another world. She was standing in a lush meadow, surrounded by towering trees and sparkling flowers. The sky was a brilliant blue, and the air was filled with the sound of birdsong.Alice wandered through the meadow, marveling at the beauty of this new world. She soon came upon a group of fairies dancing in a clearing. The fairies were surprised to see Alice, but they welcomed her with open arms.Alice spent the day playing with the fairies and learning about their magical world. She discovered that the mirror she had found in the attic was a portal to this other world. Alice was thrilled to have discovered this secret passage, and she knew that she would return to visit her new friends often.",
+        img : false
+    },
+    {
+        id : uuidv4(),
+        textData : "Detective Alex Stone had seen a lot of gruesome murders in his career, but nothing could have prepared him for what he was about to see.On a cold winter night, Alex was called to the scene of a homicide at a secluded cabin in the woods. When he arrived, he found the body of a young woman lying on the floor of the living room. She had been stabbed multiple times, and her throat had been slashed. Alex had never seen such a horrific crime scene.As he examined the body, he noticed a strange symbol carved into the victim's forehead. He had never seen the symbol before, but it gave him a sense of unease. He knew that this killer was different from any he had faced before.Alex was determined to find the killer and bring them to justice. He vowed to stop this monster before they could strike again.But as he delved deeper into the case, he began to realize that he was dealing with something more than just a human killer. He was dealing with a dark passenger, a malevolent force that was driven to kill.Alex knew that he had to face his own demons in order to defeat this evil. He knew that he was in danger, but he was also determined to stop the dark passenger at all costs.",
+        img : false
+    }
+];
+
+export {chatData};
--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -1,25 +1,38 @@
+import { useState } from "react"
+import { chatData } from "../Utils/chatData";
+
 export default function GameScreen(){
+
+    const [chats, setChats] = useState(chatData);
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (evt) =>{
+        setInputValue(evt.target.value)
+    }
+
     return (
         <section className="game-screen">
             <div className="bg-img bg-[url(/game-screen.png)] w-full h-[100vh] fixed p-4">
                 <div className="chat-area bg-white border-2 h-[100%] w-[50%] rounded-xl p-4 flex flex-col-reverse justify-start">
-                    <textarea placeholder="Enter your text here.." className="story-input h-[30%] w-full border-2 border-gray-300 outline-none rounded-md mt-2 "></textarea>
+                    <textarea value={inputValue} onChange={handleChange} placeholder="Enter your text here.." className="story-input h-[30%] w-full border-2 border-gray-300 outline-none rounded-md mt-2 overflow-y-scroll overflow-x-hidden"></textarea>
+
                     <div className="chats w-full h-[70%] border-2 rounded-md border-black p-3 flex flex-col gap-3 overflow-y-scroll">
-                        <div className="chat flex gap-1">
-                            <div className="profile w-[30px] h-[30px] rounded-[50%] bg-blue-200"></div>
-                            <div className="text w-[80%] h-44 border-2 border-gray-500 rounded-tl-none rounded-tr-lg rounded-br-lg rounded-bl-lg"></div>
-                        </div>
-                        <div className="chat flex gap-1">
-                            <div className="profile w-[30px] h-[30px] rounded-[50%] bg-blue-200"></div>
-                            <div className="text w-[80%] h-44 border-2 border-gray-500 rounded-tl-none rounded-tr-lg rounded-br-lg rounded-bl-lg"></div>
-                        </div>
-                        <div className="chat flex gap-1">
-                            <div className="profile w-[30px] h-[30px] rounded-[50%] bg-blue-200"></div>
-                            <div className="text w-[80%] h-44 border-2 border-gray-500 rounded-tl-none rounded-tr-lg rounded-br-lg rounded-bl-lg"></div>
-                        </div>
+                        {chats.map((chat) => {
+                            return (
+                            <div className="chat flex gap-3 w-[70%]">
+                                <div className="profile w-[30px] h-[30px] rounded-[50%] bg-blue-200"></div>
+                                <div className="text w-[80%] max:w-[80%] min-h-[176px] h-auto border-2 border-gray-500 rounded-tl-none rounded-tr-lg rounded-br-lg rounded-bl-lg flex px-2 text-black">
+                                    <p>{chat.textData}</p>
+                                </div>
+                             </div> 
+                            )
+                        })}
                     </div>
             </div>
             </div>
         </section>
     )
 }
+
+ 
